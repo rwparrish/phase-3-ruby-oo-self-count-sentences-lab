0cf5efb857c2b760198ea9752c1ccc33b45209ba
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-06-20 17:30:09 -0400

    working solution

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,24 @@ require 'pry'
 
 class String
 
+  def report_on_self
+    puts "self is: #{self}"
+  end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr_of_sentences = self.split(/\.|\?|!/).select { |sen| sen != "" }
+    arr_of_sentences.count
   end
 end
